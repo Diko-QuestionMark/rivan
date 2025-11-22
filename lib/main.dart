c9eb0f3ad45b0ee88267b89d77e21a690a953075
Fedriko <fedriko447@gmail.com>
2025-11-22 21:42:34 +0700

    Init project baru

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+
+
+}
+
+//1. Material Design
+//2. Cupertino Design 
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
